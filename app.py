3e397af8888190d4a09e53481c00bf809bd49c93
angela <angela.wilson739@gmail.com>
2019-08-06 21:20:39 -0400

    Adding the skeleton for adding and listing books

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 from flask import Flask
 app = Flask(__name__)
 
+books = []
 @app.route("/")
 def hello():
   return "Hello World!"
 
-@app.route("/add_book")
-def add_book(Book book):
-    pass
+@app.route("/add_book/<book>", methods=["POST"])
+def add_book(book):
+  books.append(book)
+  return "ok",200
 @app.route("/list_books")
 def list_book():
-    pass
+  for i in books:
+    return i
 
 if __name__ == "__main__":
   app.run()
